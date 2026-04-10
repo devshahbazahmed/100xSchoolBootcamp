e857f2a3f57598ae1ca6bf2afcfdf48c30ca73fc
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-04-10 22:06:34 +0530

    Complete week-12

---
+++ week-12_Trello-with-mongo/models.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+mongoose.connect("");
+
+/// schemas and models
+
+const userSchema = mongoose.Schema({
+    username: String,
+    password: String
+})
+
+const organizationSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    admin: mongoose.Types.ObjectId,
+    members: [mongoose.Types.ObjectId]
+})
+
+
+const organizationModel = mongoose.model("organizations", organizationSchema);
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = {
+    organizationModel,
+    userModel
+}
